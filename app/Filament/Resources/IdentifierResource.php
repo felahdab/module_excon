273b7750e070e9c6af915dc71d029fb2c0304e01
Florian El-Ahdab <florian.el-ahdab@intradef.gouv.fr>
2025-04-21 23:17:12 +0200

    Ajout d'un filtre par unité dans la table des identifiants.

--- app/Filament/Resources/IdentifierResource.php
+++ app/Filament/Resources/IdentifierResource.php
@@ -2,16 +2,21 @@
 
 namespace Modules\Excon\Filament\Resources;
 
-use Modules\Excon\Filament\Resources\IdentifierResource\Pages;
-use Modules\Excon\Filament\Resources\IdentifierResource\RelationManagers;
-use Modules\Excon\Models\Identifier;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Filters\Filter;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Modules\Excon\Filament\Resources\IdentifierResource\Pages;
+use Modules\Excon\Filament\Resources\IdentifierResource\RelationManagers;
+use Modules\Excon\Models\Identifier;
+use Modules\Excon\Models\Unit;
 
 class IdentifierResource extends Resource
 {
@@ -55,7 +60,25 @@ class IdentifierResource extends Resource
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Filter::make('unite')
+                    ->form([
+                        Select::make('unit_id')
+                        ->options(Unit::orderBy('name')->pluck('name', 'id')),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['unit_id'],
+                                fn (Builder $query, $unit_id): Builder => $query->where('unit_id', $unit_id),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): ?string {
+                        if (! $data['unit_id']) {
+                            return null;
+                        }
+                
+                        return 'Identifiers for unit: ' . Unit::find($data['unit_id'])->name;
+                    })
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
